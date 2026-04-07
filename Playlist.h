3be7c5041aad55268d93780b389ec5922bb27090
Connie P Ly <cly059@ucr.edu>
2026-04-07 15:35:51 -0700

    Update Playlist.h
    added function names

--- Playlist.h
+++ Playlist.h
@@ -1,1 +1,23 @@
+#ifndef PLAYLIST_H
+#define PLAYLIST_H
 
+class PlaylistNode {
+  private:
+    std::string uniqueID = "none";
+    std::string songName = "none";
+    std::string artistName = "none";
+    int songLength = 0;
+    PlaylistNode* nextNodePtr = 0;
+  public:
+    PlaylistNode();
+    PlaylistNode();
+    InsertAfter();
+    SetNext();
+    std::string GetID() const;
+    std::string GetArtistName() const;
+    int GetSongLength() const;
+    PlaylistNode* GetNext() const;
+    void PrintPlaylistNode();
+}
+
+#endif
