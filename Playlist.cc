cf5f707cade93790babab81cf2401cbe4455e008
Connie P Ly <cly059@ucr.edu>
2026-04-07 15:52:31 -0700

    Update Playlist.cc
    added template for functions

--- Playlist.cc
+++ Playlist.cc
@@ -1,1 +1,18 @@
+#include Playlist.h
+#include <iostream>
 
+//can constructor can be listed in heading?
+PlaylistNode:: PlaylistNode(): uniqueID("none"), songName("none"), artistName("none"), songLength(0), nextNodePtr(0){}
+
+PlaylistNode:: PlaylistNode();{
+
+}
+//
+PlaylistNode:: PlaylistNode();
+PlaylistNode:: InsertAfter();
+PlaylistNode:: SetNext();
+std::string PlaylistNode:: GetID() const;
+std::string PlaylistNode:: GetArtistName() const;
+std::int PlaylistNode:: GetSongLength() const;
+PlaylistNode:: PlaylistNode* GetNext() const;
+void PlaylistNode:: PrintPlaylistNode();
